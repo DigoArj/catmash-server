6f534d61d652aff49a484f2471bfbf1c994527b6
Diogo DE ARAUJO <16623572+DigoArj@users.noreply.github.com>
2020-06-20 22:10:07 +0200

    added list lambda function

---
+++ functions/list.js
@@ -0,0 +1,17 @@
+import handler from '../libs/handler-lib';
+import dynamoDb from '../libs/dynamodb-lib';
+
+export const main = handler(async () => {
+  const params = { TableName: process.env.tableName };
+
+  let items = [];
+
+  let result;
+  do {
+    result = await dynamoDb.scan(params);
+    result.Items.forEach(item => items.push(item));
+    params.ExclusiveStartKey = result.LastEvaluatedKey;
+  } while (typeof result.LastEvaluatedKey != 'undefined');
+
+  return items;
+});
